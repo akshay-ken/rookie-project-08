914b6ffd804bb7f0693a4c6f8a48fc32fa3816d5
akshay-ken <akshaybest3120@gmail.com>
2025-08-25 21:45:14 +0530

    style: add padding for mobile and desktop view

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,7 +2,7 @@ import { HeaderSection } from "./HeaderSection";
 
 export function MainPage() {
   return (
-    <main>
+    <main className="px-2 py-4 md:px-[8vw] md:py-[4vw]">
       <HeaderSection />
     </main>
   );
