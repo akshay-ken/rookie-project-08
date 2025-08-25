7cd58fd7febea2a3ec372c5ff489b804e33de614
akshay-ken <akshaybest3120@gmail.com>
2025-08-25 22:35:03 +0530

    feat: add text and tweak text styling and fix class prop

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -3,18 +3,18 @@ import imgDesktop from "../assets/images/image-web-3-desktop.jpg";
 export function ContentSection() {
   return (
     <section className="mt-6 md:mt-12">
-      <div class="flex flex-col md:grid md:grid-cols-3 md:grid-rows-2 gap-2">
+      <div className="flex flex-col md:grid md:grid-cols-3 md:grid-rows-2 gap-2">
         {/* image section */}
-        <div class="col-span-2 order-1 md:order-1">
+        <div className="col-span-2 order-1 md:order-1">
           <img src={imgMobile} className="md:hidden" alt="" />
           <img src={imgDesktop} className="hidden md:block" alt="" />
         </div>
         {/* New section */}
-        <div class="bg-rose-600 p-2 row-span-2  order-4 md:order-2">02</div>
-        <div class="text-left md:text-4xl text-2xl font-black text-Very-dark-blue order-2 md:order-3">
-          03
+        <div className="bg-rose-600 p-2 row-span-2  order-4 md:order-2">02</div>
+        <div className="text-left md:text-7xl text-5xl tracking-wide font-extrabold text-Very-dark-blue order-2 md:order-3">
+          The Bright Future of Web 3.0?
         </div>
-        <div class="bg-rose-800 p-2   order-3 md:order-4">04</div>
+        <div className="bg-rose-800 p-2   order-3 md:order-4">04</div>
       </div>
     </section>
   );
