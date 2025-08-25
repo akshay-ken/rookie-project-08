a40ad8b44c19d8130a3198e8ca384ae3af24efdb
akshay-ken <akshaybest3120@gmail.com>
2025-08-25 22:49:51 +0530

    feat: add text conent and all the styling for foruth div section

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -14,7 +14,17 @@ export function ContentSection() {
         <div className="text-left md:text-7xl text-5xl tracking-wide font-extrabold text-Very-dark-blue order-2 md:order-3">
           The Bright Future of Web 3.0?
         </div>
-        <div className="bg-rose-800 p-2   order-3 md:order-4">04</div>
+        <div className="order-3 md:order-4">
+          <p className="text-left md:text-xl text-lg  font-medium text-Dark-grayish-blue">
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+
+          <button className="bg-Soft-red text-Very-dark-blue tracking-widest uppercase font-semibold py-2 md:py-4 md:px-12 px-6 md:mt-8 mt-4 text-base md:text-xl">
+            Read more
+          </button>
+        </div>
       </div>
     </section>
   );
