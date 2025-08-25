fd152d43d40d6e9d74d21f887770ca503d1871f0
akshay-ken <akshaybest3120@gmail.com>
2025-08-25 22:07:08 +0530

    feat: add content section component with necessary code

---
+++ src/components/ContentSection.jsx
@@ -0,0 +1,12 @@
+export function ContentSection() {
+  return (
+    <section>
+      <div class="flex flex-col md:grid md:grid-cols-3 md:grid-rows-2 gap-2">
+        <div class="bg-rose-400 p-2 col-span-2  order-1 md:order-1">01</div>
+        <div class="bg-rose-600 p-2 row-span-2  order-4 md:order-2">02</div>
+        <div class="bg-rose-800 p-2  order-2 md:order-3">03</div>
+        <div class="bg-rose-800 p-2   order-3 md:order-4">04</div>
+      </div>
+    </section>
+  );
+}
