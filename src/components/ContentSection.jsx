392ecf292adf0bba9f4ecee0e6866a6a2dd0f0b9
akshay-ken <akshaybest3120@gmail.com>
2025-08-25 22:27:42 +0530

    style: show image depending on mobile or desktop view

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -4,7 +4,12 @@ export function ContentSection() {
   return (
     <section className="mt-6 md:mt-12">
       <div class="flex flex-col md:grid md:grid-cols-3 md:grid-rows-2 gap-2">
-        <div class="bg-rose-400 p-2 col-span-2  order-1 md:order-1">01</div>
+        {/* image section */}
+        <div class="col-span-2  order-1 md:order-1">
+          <img src={imgMobile} className="md:hidden" alt="" />
+          <img src={imgDesktop} className="hidden md:block" alt="" />
+        </div>
+        {/* New section */}
         <div class="bg-rose-600 p-2 row-span-2  order-4 md:order-2">02</div>
         <div class="bg-rose-800 p-2  order-2 md:order-3">03</div>
         <div class="bg-rose-800 p-2   order-3 md:order-4">04</div>
