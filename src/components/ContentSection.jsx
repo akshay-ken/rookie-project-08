a21ab7ce7442be764fbb43d5d214959ca20cd248
akshay-ken <akshaybest3120@gmail.com>
2025-08-25 22:10:05 +0530

    style: add top margin for both views

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -1,6 +1,6 @@
 export function ContentSection() {
   return (
-    <section>
+    <section className="mt-6 md:mt-12">
       <div class="flex flex-col md:grid md:grid-cols-3 md:grid-rows-2 gap-2">
         <div class="bg-rose-400 p-2 col-span-2  order-1 md:order-1">01</div>
         <div class="bg-rose-600 p-2 row-span-2  order-4 md:order-2">02</div>
