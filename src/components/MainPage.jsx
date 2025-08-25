91a9c28e5540354f881a83b158d279867654c5d8
akshay-ken <akshaybest3120@gmail.com>
2025-08-25 22:07:54 +0530

    feat: use content section component in mainPage.jsx file

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,9 +1,11 @@
+import { ContentSection } from "./ContentSection";
 import { HeaderSection } from "./HeaderSection";
 
 export function MainPage() {
   return (
     <main className="px-2 py-4 md:px-[8vw] md:py-[4vw]">
       <HeaderSection />
+      <ContentSection />
     </main>
   );
 }
