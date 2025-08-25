7dca35e77f458d499949e59f1fb7e1b526dc6282
akshay-ken <akshaybest3120@gmail.com>
2025-08-25 22:30:12 +0530

    style: add text styling

--- src/components/ContentSection.jsx
+++ src/components/ContentSection.jsx
@@ -5,13 +5,15 @@ export function ContentSection() {
     <section className="mt-6 md:mt-12">
       <div class="flex flex-col md:grid md:grid-cols-3 md:grid-rows-2 gap-2">
         {/* image section */}
-        <div class="col-span-2  order-1 md:order-1">
+        <div class="col-span-2 order-1 md:order-1">
           <img src={imgMobile} className="md:hidden" alt="" />
           <img src={imgDesktop} className="hidden md:block" alt="" />
         </div>
         {/* New section */}
         <div class="bg-rose-600 p-2 row-span-2  order-4 md:order-2">02</div>
-        <div class="bg-rose-800 p-2  order-2 md:order-3">03</div>
+        <div class="text-left md:text-4xl text-2xl font-black text-Very-dark-blue order-2 md:order-3">
+          03
+        </div>
         <div class="bg-rose-800 p-2   order-3 md:order-4">04</div>
       </div>
     </section>
