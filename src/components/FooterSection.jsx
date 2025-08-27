f1df1c26a21097bcdd5df853d2cdbecaef4e0a95
akshay-ken <akshaybest3120@gmail.com>
2025-08-27 13:28:00 +0530

    feat: add text content of secnd child element

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -18,12 +18,12 @@ export function FooterSection() {
       <div className="flex flex-row gap-6 md:basis-1/3">
         <img src={keyImg} className="w-28" alt="" />
         <div className="flex flex-col justify-around">
-          <p className="text-3xl font-extrabold text-Soft-red">01</p>
+          <p className="text-3xl font-extrabold text-Soft-red">02</p>
           <p className="text-2xl font-bold text-Very-dark-blue">
-            Reviving Retro PCs
+            Top 10 Laptops of 2022
           </p>
           <p className="text-xl font-medium text-Dark-grayish-blue">
-            What happens when old PCs are given modern upgrades?
+            Our best picks for various needs and budgets.
           </p>
         </div>
       </div>
