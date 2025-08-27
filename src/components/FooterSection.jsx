7b3cff9a07ad6bd74d151ff7a950e8a06a554533
akshay-ken <akshaybest3120@gmail.com>
2025-08-27 13:13:27 +0530

    sytle: add img tag with img and its fixed sizing

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,8 +1,9 @@
+import pcImg from "../assets/images/image-retro-pcs.jpg";
 export function FooterSection() {
   return (
     <footer className="flex flex-col md:flex-row">
       <div className="flex flex-row">
-        <img src="" alt="" />
+        <img src={pcImg} className="w-28" alt="" />
         <div className="flex flex-col"></div>
       </div>
     </footer>
