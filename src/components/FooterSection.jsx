550662db258938cf13244305661428569c7bc954
akshay-ken <akshaybest3120@gmail.com>
2025-08-27 13:09:54 +0530

    style: add flex stying to component

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,7 +1,10 @@
 export function FooterSection() {
   return (
-    <footer>
-      <div></div>
+    <footer className="flex flex-col md:flex-row">
+      <div className="flex flex-row">
+        <img src="" alt="" />
+        <div className="flex flex-col"></div>
+      </div>
     </footer>
   );
 }
