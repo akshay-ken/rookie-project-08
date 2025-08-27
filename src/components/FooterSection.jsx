1636c20412b5d577d79e57b7ec97be0a7ab8b6ef
akshay-ken <akshaybest3120@gmail.com>
2025-08-27 13:20:24 +0530

    style: give styling to both flex container

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -2,9 +2,17 @@ import pcImg from "../assets/images/image-retro-pcs.jpg";
 export function FooterSection() {
   return (
     <footer className="flex flex-col md:flex-row">
-      <div className="flex flex-row">
+      <div className="flex flex-row gap-6">
         <img src={pcImg} className="w-28" alt="" />
-        <div className="flex flex-col"></div>
+        <div className="flex flex-col justify-around">
+          <p className="text-3xl font-extrabold text-Soft-red">01</p>
+          <p className="text-2xl font-bold text-Very-dark-blue">
+            Reviving Retro PCs
+          </p>
+          <p className="text-xl font-medium text-Dark-grayish-blue">
+            What happens when old PCs are given modern upgrades?
+          </p>
+        </div>
       </div>
     </footer>
   );
