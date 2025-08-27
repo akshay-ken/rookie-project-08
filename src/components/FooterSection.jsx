c1f965efa0f25abdbc89c275b2339b0e8ab6e7e3
akshay-ken <akshaybest3120@gmail.com>
2025-08-27 13:22:13 +0530

    style: add md basis and margin for mobile

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,8 +1,8 @@
 import pcImg from "../assets/images/image-retro-pcs.jpg";
 export function FooterSection() {
   return (
-    <footer className="flex flex-col md:flex-row">
-      <div className="flex flex-row gap-6">
+    <footer className="flex flex-col md:flex-row mt-12 md:mt-0">
+      <div className="flex flex-row gap-6 md:basis-1/3">
         <img src={pcImg} className="w-28" alt="" />
         <div className="flex flex-col justify-around">
           <p className="text-3xl font-extrabold text-Soft-red">01</p>
