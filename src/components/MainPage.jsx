3c90da047483c28dfcb512d0e3614afa196731c2
akshay-ken <akshaybest3120@gmail.com>
2025-08-28 18:51:34 +0530

    style: use relative and absolute to have overlay

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -4,7 +4,7 @@ import { HeaderSection } from "./HeaderSection";
 
 export function MainPage() {
   return (
-    <main className="px-2 py-4 md:px-[8vw] md:py-[4vw]">
+    <main className="px-2 py-4 md:px-[8vw] md:py-[4vw] relative">
       <HeaderSection />
       <ContentSection />
       <FooterSection />
--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,8 +1,11 @@
 import { createPortal } from "react-dom";
 export function MobileMenu({ onClose }) {
   return createPortal(
-    <div onClick={onClose}>
-      <div></div>
+    <div
+      onClick={onClose}
+      className="h-screen absolute top-0 bg-Dark-grayish-blue w-full"
+    >
+      <div>test</div>
       <ul></ul>
     </div>,
     document.getElementById("menu")
