97bdb01cf61c6a721b2fdc317281e9ea80b35cc7
akshay-ken <akshaybest3120@gmail.com>
2025-08-28 19:09:50 +0530

    feat: prevent menu close on child element using onclick prop

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -5,7 +5,12 @@ export function MobileMenu({ onClose }) {
       onClick={onClose}
       className="h-screen fixed inset-0 bg-Dark-grayish-blue/40 bg-blend-overlay p-6 w-full"
     >
-      <div className="w-2/3 bg-Off-white absolute inset-y-0 right-0">test</div>
+      <div
+        className="w-2/3 bg-Off-white absolute inset-y-0 right-0"
+        onClick={(e) => e.stopPropagation()}
+      >
+        test
+      </div>
       <ul></ul>
     </div>,
     document.getElementById("menu")
